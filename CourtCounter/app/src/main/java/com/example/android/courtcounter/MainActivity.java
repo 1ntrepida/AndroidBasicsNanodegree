7005c2a836417fde25bee02c4e92a0297054c119
1ntrepida <alexa.rockwell@stonybrook.edu>
2016-07-04 04:51:44 -0400

    completed rough draft of basket ball counter program

---
+++ CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.android.courtcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int playerOne = 0;
+    int playerTwo = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void p1One(View view)
+    {
+        playerOne++;
+        displayP1(playerOne);
+    }
+
+    public void p1Two(View view)
+    {
+        playerOne+=2;
+        displayP1(playerOne);
+    }
+
+    public void p1Three(View view)
+    {
+        playerOne+=3;
+        displayP1(playerOne);
+    }
+
+    public void p2One(View view)
+    {
+        playerTwo++;
+        displayP2(playerTwo);
+    }
+
+    public void p2Two(View view)
+    {
+        playerTwo+=2;
+        displayP2(playerTwo);
+    }
+
+    public void p2Three(View view)
+    {
+        playerTwo+=3;
+        displayP2(playerTwo);
+    }
+
+    private void displayP1(int number)
+    {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.player1);
+        quantityTextView.setText(""+number);
+    }
+
+    private void displayP2(int number)
+    {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.player2);
+        quantityTextView.setText(""+number);
+    }
+
+    public void reset(View view)
+    {
+        playerOne = 0;
+        playerTwo = 0;
+        displayP1(playerOne);
+        displayP2(playerTwo);
+    }
+
+}
+
