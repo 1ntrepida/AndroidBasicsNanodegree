7c4063cf763d6f2db57bf7d97981477d4a6d8052
1ntrepida <alexa.rockwell@stonybrook.edu>
2016-07-05 00:16:54 -0400

    created xml layout

--- Cookies/app/src/main/java/com/example/android/cookies/MainActivity.java
+++ Cookies/app/src/main/java/com/example/android/cookies/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.cookies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * Called when the cookie should be eaten.
+     */
+    public void eatCookie(View view) {
+        // TODO: Find a reference to the ImageView in the layout. Change the image.
+        displayImage();
+        displayText();
+        // TODO: Find a reference to the TextView in the layout. Change the text.
+
+    }
+
+    private void displayImage() {
+        ImageView cookie = (ImageView) findViewById(R.id.android_cookie_image_view);
+        cookie.setImageResource(R.drawable.after_cookie);
+    }
+
+    private void displayText() {
+        TextView full = (TextView) findViewById(R.id.status_text_view);
+        full.setText("I'm so full");
+
+    }
+
 }
