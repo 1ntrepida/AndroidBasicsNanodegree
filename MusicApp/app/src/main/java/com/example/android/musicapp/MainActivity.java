78030d758243ac9dc5cb757e094176340d81e053
1ntrepida <alexa.rockwell@stonybrook.edu>
2016-07-08 22:30:00 -0400

    added onclicklistener

--- MusicApp/app/src/main/java/com/example/android/musicapp/MainActivity.java
+++ MusicApp/app/src/main/java/com/example/android/musicapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.musicapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView nowPlaying = (TextView) findViewById(R.id.nowPlaying);
+
+        // Set a click listener on that View
+        nowPlaying.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent numbersIntent = new Intent(MainActivity.this, NowPlayingActivity.class);
+
+                // Start the new activity
+                startActivity(numbersIntent);
+            }
+        });
+
     }
 }
---
+++ MusicApp/app/src/main/java/com/example/android/musicapp/NowPlayingActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.musicapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Alexa on 7/8/2016.
+ */
+public class NowPlayingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nowplaying);
+    }
+}
