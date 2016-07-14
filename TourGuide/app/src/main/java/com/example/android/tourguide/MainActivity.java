98f939de26c9e11c34118732c847c677dff1d791
1ntrepida <alexa.rockwell@stonybrook.edu>
2016-07-13 23:41:27 -0400

    created the locations class

--- TourGuide/app/src/main/java/com/example/android/tourguide/MainActivity.java
+++ TourGuide/app/src/main/java/com/example/android/tourguide/MainActivity.java
@@ -10,4 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public class Locations() {
+        //will serve as a place to
+    }
+
 }
