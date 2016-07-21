91bd3e4eb267de873b167b7d84d11f06a8525bc4
1ntrepida <alexa.rockwell@stonybrook.edu>
2016-07-21 15:03:37 -0400

    changed structure

---
+++ TourGuide/app/src/main/java/com/example/android/tourguide/LocationAdapter.java
@@ -0,0 +1,43 @@
+package com.example.android.tourguide;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Alexa on 7/21/2016.
+ */
+public class LocationAdapter extends ArrayAdapter<Location>{
+
+    public LocationAdapter(Context context, ArrayList<Location> locations) {
+        super(context, 0, locations);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if an existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // Get the {@link Word} object located at this position in the list
+        Location currentLocation = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID miwok_text_view.
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        // Get the Miwok translation from the currentWord object and set this text on
+        // the Miwok TextView.
+        miwokTextView.setText(currentLocation.getName());
+
+        // Return the whole list item layout (containing 2 TextViews) so that it can be shown in
+        // the ListView.
+        return listItemView;
+    }
+}
--- TourGuide/app/src/main/java/com/example/android/tourguide/TourFragment.java
+++ TourGuide/app/src/main/java/com/example/android/tourguide/TourFragment.java
@@ -20,11 +20,14 @@ import java.util.List;
 public class TourFragment extends Fragment {
 
     private ArrayAdapter<String> mLocationAdapter;
-    private ArrayList<String> locations;
+    private ArrayList<Location> locations;
 
     public TourFragment(){
-        locations = new ArrayList<String>();
-        locations.add(new Location(R.drawable.1 ,"Times Square"));
+        locations = new ArrayList<Location>();
+        locations.add(new Location(R.drawable.one ,"Times Square", "Epicenter of New York hustle and bustle."));
+        locations.add(new Location(R.drawable.two ,"Museum of Modern Art", "Gorgeous collection of modern art."));
+        locations.add(new Location(R.drawable.three ,"9/11 Memorial", "The monument paying tribute to the lives lost at 9/11/2001"));
+        locations.add(new Location(R.drawable.four ,"Statue of Liberty", "The Statue of Liberty is a colossal neoclassical sculpture on Liberty Island in New York Harbor in New York City, in the United States."));
     }
 
     @Override
@@ -35,48 +38,10 @@ public class TourFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-        // Create some dummy data for the ListView.  Here's a sample weekly forecast
-        String[] data = {
-                "Mon 6/23 - Sunny - 31/17",
-                "Tue 6/24 - Foggy - 21/8",
-                "Wed 6/25 - Cloudy - 22/17",
-                "Thurs 6/26 - Rainy - 18/11",
-                "Fri 6/27 - Foggy - 21/10",
-                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-                "Sun 6/29 - Sunny - 20/7"
-        };
-        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
-
-        // Now that we have some dummy forecast data, create an ArrayAdapter.
-        // The ArrayAdapter will take data from a source (like our dummy forecast) and
-        // use it to populate the ListView it's attached to.
-        mForecastAdapter =
-                new ArrayAdapter<String>(
-                        getActivity(), // The current context (this activity)
-                        R.layout.list_item_forecast, // The name of the layout ID.
-                        R.id.list_item_forecast_textview, // The ID of the textview to populate.
-                        weekForecast);
-
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-        // Get a reference to the ListView, and attach this adapter to it.
-        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-        listView.setAdapter(mForecastAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                String forecast = mForecastAdapter.getItem(position);
-                Intent intent = new Intent(getActivity(), DetailActivity.class)
-                        .putExtra(Intent.EXTRA_TEXT, forecast);
-                startActivity(intent);
-            }
-        });
-
-        return rootView;
+    public View onCreateView(Bundle savedInstanceState) {
+        LocationAdapter adapter = new LocationAdapter();
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
     }
 
 }
