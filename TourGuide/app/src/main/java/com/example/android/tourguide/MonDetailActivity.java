03f04e5e66e1eb9439afffb0237b7c5760e2f747
1ntrepida <alexa.rockwell@stonybrook.edu>
2016-07-27 05:33:22 -0400

    working with intents in detail screen

---
+++ TourGuide/app/src/main/java/com/example/android/tourguide/MonDetailActivity.java
@@ -0,0 +1,46 @@
+package com.example.android.tourguide;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Alexa on 7/27/2016.
+ */
+public class MonDetailActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+    /**
+     * +     * A placeholder fragment containing a simple view.
+     * +
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            View rootView = inflater.inflate(R.layout.location_detail, container, false);
+            return rootView;
+        }
+    }
+}
